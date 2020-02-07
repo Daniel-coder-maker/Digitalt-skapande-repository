a7a269f325a954803eef8d93412109c366fc8c98
Mattis Bohman <devmattisb@gmail.com>
2020-02-07 13:16:29 +0100

    Add PlayerController script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [NonSerialized]
+    public static PlayerController instance;
+
+    // Player movement speed
+    public float Speed = 1f;
+    private void Start()
+    {
+        instance = this;
+    }
+
+    public void FixedUpdate()
+    {
+        // Movement
+        transform.position += Time.fixedDeltaTime * Speed * new Vector3(Input.GetAxis("Horizontal"),0f,Input.GetAxis("Vertical"));
+    }
+}
